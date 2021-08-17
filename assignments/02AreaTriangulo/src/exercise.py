08b64d47f6ef893693ac50cfe5ffa686f74d7a8a
A01742457 <a01742457@tec.mx>
2021-08-17 13:31:34 +0000

    Tarea resuelta

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    pass 
+    b = int(input("Dame la base: "))
+    a = int(input("Dame la altura: "))
+    
+    area= (b*a)/ 2
+    print ("El área es: " +str(area))
+
+    
 
 if __name__ == '__main__':
     main()
